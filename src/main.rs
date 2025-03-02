671bf5d2523d2f0520f7fcd739c0809df7884fc7
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2025-03-02 14:32:55 +0100

    feat: Copy natvis file to selected directories
    - Handle multiple natvis versions (Qt5 or Qt6 for now)
    - Handle errors nicely

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use console::style;
 use std::path::PathBuf;
+use std::{env, fs};
 
 /// Struct to store natvis information for installation.
 struct NatvisInfo {
@@ -9,10 +10,14 @@ struct NatvisInfo {
     name: String,
     /// Path to the directory
     path: PathBuf,
+    /// Version(s) to install
+    version: Vec<u8>,
 }
 
 fn main() {
     cliclack::intro(style("Natvis installation for Qt").on_green().black()).unwrap();
+
+    cliclack::log::info(format!("This tool will install natvis files for Qt and MSVC\nExisting natvis files will be {}!", style("overwritten").yellow().bold())).unwrap();
 
     let qt_root = ui_get_qt_root();
     if qt_root.is_err() {
@@ -28,7 +33,17 @@ fn main() {
         return;
     }
 
-    cliclack::outro(style("Success!").green().bold()).unwrap();
+    let natvis_installs = natvis_installs
+        .iter()
+        .filter(|info| install_keys.as_ref().unwrap().contains(&info.key.as_str()))
+        .collect::<Vec<_>>();
+
+    let success = ui_install_natvis_files(natvis_installs);
+
+    match success {
+        Ok(_) => cliclack::outro(style("Success!").green().bold()).unwrap(),
+        Err(_) => ui_outro_error(),
+    }
 }
 
 /// Returns all the MS Visual Studio user directories.
@@ -48,6 +63,7 @@ fn get_msvc_dirs() -> Vec<NatvisInfo> {
                 key: key.to_string(),
                 name: name.to_string(),
                 path: vs_dir.join("Visualizers"),
+                version: vec![5, 6],
             });
         }
     }
@@ -77,6 +93,13 @@ fn get_qt_dirs(qt_root: PathBuf) -> Vec<NatvisInfo> {
         // If it has a child directory starting with msvc, add it to the list
         let qt_version_dir = qt_root.join(&version);
 
+        let qt_version_major = version
+            .split('.')
+            .next()
+            .expect("Could not extract Qt major version")
+            .parse::<u8>()
+            .expect("Could not parse Qt major version");
+
         let qt_msvc_dirs = qt_version_dir
             .read_dir()
             .expect("Could not read Qt version directory")
@@ -93,6 +116,7 @@ fn get_qt_dirs(qt_root: PathBuf) -> Vec<NatvisInfo> {
                     qt_version
                 },
                 path: qt_version_dir.join(entry).join("natvis"),
+                version: vec![qt_version_major],
             });
 
         dirs.extend(qt_msvc_dirs);
@@ -152,7 +176,7 @@ fn ui_get_install_keys(dirs: &[NatvisInfo]) -> Result<Vec<&str>, std::io::Error>
         })
         .collect::<Vec<_>>();
 
-    cliclack::multiselect("Select known directories to install natvis files")
+    cliclack::multiselect("Select directories to install natvis files")
         .initial_values(keys)
         .items(&dirs_for_multiselect)
         .interact()
@@ -168,4 +192,66 @@ fn ui_outro_error() {
     ))
     .unwrap();
     std::process::exit(0)
+}
+
+/// UI: install the natvis files.
+fn ui_install_natvis_files(natvis_installs: Vec<&NatvisInfo>) -> Result<(), std::io::Error> {
+    let spinner = cliclack::spinner();
+    spinner.start("Copying natvis files...");
+
+    let mut errors = Vec::new();
+    for info in &natvis_installs {
+        spinner.set_message(format!("Copying natvis files for {}", info.name).as_str());
+        cliclack::log::info(format!("Copying natvis files for {}", info.name))?;
+        for version in info.version.iter() {
+            if let Err(e) = copy_natvis_file(*version, info.path.clone()) {
+                errors.push(e);
+            }
+        }
+    }
+
+    match errors.is_empty() {
+        true => spinner.stop(format!("Copied {} natvis files", natvis_installs.len())),
+        _ => {
+            spinner.clear();
+            for e in errors {
+                cliclack::log::error(format!("{}", e)).unwrap();
+            }
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Some errors occurred during the installation",
+            ));
+        }
+    }
+
+    Ok(())
+}
+
+/// Copy natvis file to the given path.
+fn copy_natvis_file(version: u8, dst: PathBuf) -> Result<(), std::io::Error> {
+    let natvis_file_name = format!("qt{}.natvis", version);
+
+    // Create directory unconditionally
+    if fs::create_dir_all(&dst).is_err() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Could not create {}", dst.display()),
+        ));
+    }
+
+    // Get executable directory
+    let src = env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("natvis")
+        .join(&natvis_file_name);
+
+    if fs::copy(src, dst.join(&natvis_file_name)).is_err() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Could not copy {} to {}", natvis_file_name, dst.display()),
+        ));
+    }
+    Ok(())
 }
